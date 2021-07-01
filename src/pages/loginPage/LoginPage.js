388cd500dc51b71dd25cf1fd4a2bef851998ab02
Glodi Mboko <gmboko664@gmail.com>
2021-07-01 21:28:20 +0100

    feature : title creation

---
+++ src/pages/loginPage/LoginPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Title from "../../components/title/Title";
+const LoginPage = () => {
+  return (
+    <div className="container">
+      <Title />
+    </div>
+  );
+};
+
+export default LoginPage;
