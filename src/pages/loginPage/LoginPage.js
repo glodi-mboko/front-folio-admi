6f897c5a0dba753c6050224909db9cc36ed2d25b
Glodi Mboko <gmboko664@gmail.com>
2021-07-19 17:23:18 +0100

    style:stylize the login container

--- src/pages/loginPage/LoginPage.js
+++ src/pages/loginPage/LoginPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Login from "../../components/container_login/ContainerLogin";
 import Title from "../../components/title/Title";
 const LoginPage = () => {
   return (
     <div className="container">
       <Title />
+      <Login/>
     </div>
   );
 };
