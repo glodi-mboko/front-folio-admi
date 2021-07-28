0e9cba121876b4cf8f91bda7f9f7c0f020eaa046
Glodi Mboko <gmboko664@gmail.com>
2021-07-28 22:07:26 +0100

    style:stilyze head link administration content

--- src/components/content_admi/ContentHeadAdmi.js
+++ src/components/content_admi/ContentHeadAdmi.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Search from "../../images/svg/search.svg";
+import { NavLink } from "react-router-dom";
 const ContentHeadAdmi = () => {
   return (
     <div className="content-admi__head">
-      <div className="label-link">ALL PROJECTS</div>
-      <div className="label-link">NEW PROJECT</div>
       <div className="label-link">
-        MESSAGES <div className="label-link__msg-number">5</div>
+        <NavLink activeClassName="active" to="/admi">
+          ALL PROJECTS
+        </NavLink>
       </div>
-      <div className="label-link">SETTING</div>
+      <div className="label-link">
+        <NavLink to="/adm">NEW PROJECT</NavLink>
+      </div>
+      <div className="label-link">
+        <NavLink to="/ad">MESSAGES</NavLink>
+        <div className="label-link__msg-number">5</div>
+      </div>
+      <div className="label-link">
+        <NavLink to="/a">SETTING</NavLink>
+      </div>
       <div className="search-zone">
         <img src={Search} alt="search" />
         <input type="text" placeholder="" />
